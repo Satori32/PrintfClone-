c8eccd941b521d11f66088c220eb724890634f1e
Satori32 <Huga8989+githubf@gmail.com>
2019-07-11 00:29:12 +0900

    プロジェクト ファイルを追加します。

---
+++ PrintfClone？？？/PrintF.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <cstdint>
+/**/
+std::uintmax_t PrintF_Show(const std::string_view& Fmt) {
+	std::cout << Fmt;
+	return 0;
+}
+/**/
+
+std::string to_string(const std::string_view& In) {
+	return In.data();
+}
+
+template<class T>
+std::uintmax_t PrintF_Show(const std::string_view& Fmt, const T& In) {
+		std::size_t N = 0;
+
+	N = Fmt.find_first_of("%", 0);
+	
+	if (N == Fmt.npos) return 0;
+
+	const std::string_view FMT = Fmt.substr(0, N);
+	using std::to_string;
+	std::string O = to_string(In);	
+	std::cout << FMT<<O;
+
+	PrintF_Show(Fmt.substr(N+1, Fmt.size()));
+
+	return FMT.size()+O.size();
+}
+
+template<class T,class ... Args>
+std::uintmax_t PrintF_Show(const std::string_view& Fmt, const T& In, const Args&... A) {
+	std::size_t N = 0;
+
+	N = Fmt.find_first_of("%", 0);
+
+
+	if (N == Fmt.npos) return 0;
+
+	const std::string_view FMT = Fmt.substr(0, N);
+	using std::to_string;
+	//std::string O = to_string( In );
+
+
+	std::cout << FMT<<In;
+
+	PrintF_Show(Fmt.substr(N+1, Fmt.size()) , A...);
+
+	return FMT.size();// +O.size();
+}
+
+
+template<class ... Args>
+std::uintmax_t PrintF(const std::string_view& Fmt, const Args&... In) {
+	return PrintF_Show(Fmt, In...);
+}
---
+++ PrintfClone？？？/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "PrintF.h"
+
+int main() {
+
+
+	//PrintF("I am %. %Year's Old.",  "yakitori", 18);
+	PrintF("私は%です。%歳です。","ヤキトリ",36);
+
+	return 0;
+}
